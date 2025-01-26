6b3cbd1471069fba02f347d940121d0df1efcbd8
Vivek-c29 <vivekchauhan4773@gmail.com>
2025-01-26 13:14:06 +0530

    Initial commit

---
+++ temp.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import sklearn
+import joblib
+import pickle
+
+print('Numpy',np.__version__)
+print('pandas',pd.__version__)
+print('streamlit',st.__version__)
+print('sklearn',sklearn.__version__)
+print('joblib',joblib.__version__)
+# print('pickle',pickle.__version__)
